e0241d6abbce727066a829f52f2d8f6b2f7e77be
muhsin <muhsintp.develop@gmail.com>
2025-07-28 15:53:19 +0530

    changes on localhost 8080

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 const http = require('http');
 const mongoose = require('mongoose');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 
 const server = http.createServer(app);
